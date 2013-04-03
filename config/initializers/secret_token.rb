c278d1cd45fe30314b37ba970e73ec77081566ea
dbc gogo <dbc@dbc.com>
2013-04-03 09:50:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcOverflow::Application.config.secret_token = 'd8b72178004efdfe6a042525455c5da9d3d8f44087da1678334b1affa2403af2a31ec06a71817244a2aceb02f75c89c66281b59d8990a9906a4d1a6a2c952589'
